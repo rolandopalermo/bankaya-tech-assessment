c39b5cdab2885ad6976fee4f2728257accac76cf
rolandopalermo <support@rolandopalermo.com>
2022-06-05 22:30:34 -0500

    first commit

---
+++ src/main/java/mx/com/bankaya/bankayatechassessment/BakayaTechAssessmentApplication.java
@@ -0,0 +1,13 @@
+package mx.com.bankaya.bankayatechassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakayaTechAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakayaTechAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/bankaya/bankayatechassessment/BakayaTechAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.bankaya.bankayatechassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakayaTechAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
